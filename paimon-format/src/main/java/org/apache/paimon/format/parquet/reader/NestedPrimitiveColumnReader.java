c1928af9e3cf0e4b688a099d1103578bfa3f79e3
yuzelin <zelin.yzl@alibaba-inc.com>
2024-12-20 03:00:44 +0800

    try-fix-1

--- paimon-format/src/main/java/org/apache/paimon/format/parquet/reader/NestedPrimitiveColumnReader.java
+++ paimon-format/src/main/java/org/apache/paimon/format/parquet/reader/NestedPrimitiveColumnReader.java
@@ -285,11 +285,13 @@ public class NestedPrimitiveColumnReader implements ColumnReader<WritableColumnV
             } else {
                 if (readMapKey) {
                     lastValue.skip();
+                } else if (readRowField && maxDefLevel <= 3) {
+                    lastValue.setValue(null);
                 } else {
                     if (definitionLevel == maxDefLevel - 1) {
                         // null value inner set
                         lastValue.setValue(null);
-                    } else if (definitionLevel >= maxDefLevel - 3 && readRowField) {
+                    } else if (definitionLevel >= maxDefLevel - 2 && readRowField) {
                         lastValue.setValue(null);
                     } else {
                         // current set is empty or null
